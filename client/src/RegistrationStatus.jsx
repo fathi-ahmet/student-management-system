a5faa953cf286435fd07c2c7e1024ce649bdda98
Fathi Ahmed <fathiahmet9@gmail.com>
2026-09-26 10:00:32 +0300

    Add CSS import for styling the registration status page

--- client/src/RegistrationStatus.jsx
+++ client/src/RegistrationStatus.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import "./RegistrationStatus.css";
 
 const API_URL = "http://localhost:5000/api";
 
